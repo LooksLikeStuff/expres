cf8e20d8bf49707ce1394ef97bc739c5202448b6
LooksLikeStuff <thetroop3r1337@gmail.com>
2025-08-05 19:00:12 +0700

    Feature: add onlyActive scope to userchat

--- app/Models/UserChat.php
+++ app/Models/UserChat.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\OnlyActiveScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +16,9 @@ class UserChat extends Model
         'joined_at',
         'left_at',
     ];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new OnlyActiveScope);
+    }
 }
